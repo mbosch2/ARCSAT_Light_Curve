8c188240d2ee9ca404b5734f5a30e57cec8e4c6b
mbosch2 <159995414+mbosch2@users.noreply.github.com>
2025-06-11 22:31:16 -0700

    Update Dark.py

--- Dark.py
+++ Dark.py
@@ -1,18 +1,30 @@
+import numpy
+import matplotlib.pyplot as plt
+from astropy.io import fits
+from astropy.stats import sigma_clip
+from astropy.visualization import ImageNormalize, LinearStretch, ZScaleInterval
+from astroscrappy import detect_cosmics
+from astropy.stats import sigma_clipped_stats
+from photutils.aperture import CircularAperture
+from photutils.aperture import aperture_photometry
+from photutils.aperture import CircularAnnulus
+from photutils.aperture import ApertureStats
+from photutils.profiles import RadialProfile
+from photutils.centroids import centroid_1dg
 def create_median_dark(dark_list, bias_filename, median_dark_filename):
-    """This function must:
-
-    - Accept a list of dark file paths to combine as dark_list.
-    - Accept a median bias frame filename as bias_filename (the one you created using
-      create_median_bias).
-    - Read all the images in dark_list and create a list of 2D numpy arrays.
-    - Read the bias frame.
-    - Subtract the bias frame from each dark image.
-    - Divide each dark image by its exposure time so that you get the dark current
-      per second. The exposure time can be found in the header of the FITS file.
-    - Use a sigma clipping algorithm to combine all the bias-corrected dark frames
-      using the median and removing outliers outside 3-sigma for each pixel.
-    - Save the resulting dark frame to a FITS file with the name median_dark_filename.
-    - Return the median dark frame as a 2D numpy array.
+    """
+    - Accepts a list of dark file paths to combine as dark_list.
+    - Accepts a median bias frame filename as bias_filename (the one you created using
+      Bias.py).
+    - Reads all the images in dark_list and create a list of 2D numpy arrays.
+    - Reads the bias frame.
+    - Subtracts the bias frame from each dark image.
+    - Divides each dark image by its exposure time so that you get the dark current
+      per second.
+    - Uses a sigma clipping algorithm to combine all the bias-corrected dark frames
+      using the median and removes outliers outside 3-sigma for each pixel.
+    - Saves the resulting dark frame to a FITS file with the name median_dark_filename.
+    - Returns the median dark frame as a 2D numpy array.
 
     """
     dark_images=[]
